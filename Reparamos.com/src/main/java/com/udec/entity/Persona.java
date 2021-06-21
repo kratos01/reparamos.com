cae89f6df49217c701fb1bc30e4ea7f8555d779f
Julian Arias <juliandavid_159@hotmail.com>
2021-06-21 16:13:23 -0500

    cambios

--- Reparamos.com/src/main/java/com/udec/entity/Persona.java
+++ Reparamos.com/src/main/java/com/udec/entity/Persona.java
@@ -5,7 +5,10 @@ public class Persona {
 	private String nombre;
 	private String apellido;
 	private int cedula;
+	private int telefono;
+	private String direccion;
 	private String fechaNacimiento;
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -23,6 +26,18 @@ public class Persona {
 	}
 	public void setCedula(int cedula) {
 		this.cedula = cedula;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 	public String getFechaNacimiento() {
 		return fechaNacimiento;
